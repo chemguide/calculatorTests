84b2333d5026b934ec8975cf346c5129e138091f
chemguide <argentum198@gmail.com>
2024-08-13 21:53:31 +0500

    Initial commit

---
+++ src/main/java/com/example/calculatorTests/CalculatorTestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.calculatorTests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/calculatorTests/CalculatorTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculatorTests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
